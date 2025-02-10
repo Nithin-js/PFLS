34faccc1372cd642fbf3ac71692f6f9fe18bb85f
Nithin Joseph Simon <nithinjosephsimon@gmail.com>
2025-02-10 11:42:24 +0100

    Gc content rectified

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,6 +1,6 @@
 num_sequences=$(grep -c '>' $1)
 # To count the total length of the sequences
-total_length=$(awk '/^>/ {next} {totallength += length $0} END{print totallength}' $1)
+total_length=$(grep -v '>' $1 | wc -c)
 #Longest seq
 length_of_the_longest_sequence=$(awk '/>/ {if (seq) print seq; print; seq=""; next} {seq=seq $0} END {print seq}' $1 | grep -v '>'| awk '{print length($0)}' | sort -n | tail -n 1)
 #Shortest seq
@@ -8,12 +8,13 @@ length_of_the_shortest_sequence=$(awk '/>/ {if (seq) print seq; print; seq=""; n
 #GC count
 gc_count=$(grep -v '>' $1 | awk '{gc_count += gsub(/[GgCc]/, "", $1)} END {print gc_count}')
 
+gc_content=$(echo "scale=2; ($gc_count/$total_length)*100" | bc)
 
 echo "FASTA File Statistics:"
-echo "----------------------"
 echo "Number of sequences: $num_sequences"
 echo "Total length of sequences: $total_length"
 echo "Length of the longest sequence: $length_of_the_longest_sequence" 
 echo "Length of the shortest sequence: $length_of_the_shortest_sequence"
 echo "Average sequence length: $(($total_length/$num_sequences))"
-echo "GC Content (%): $((100* $gc_count/$total_length)) %"
+echo "GC Content (%): $gc_content"
+ 
