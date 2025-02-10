3762354b2177920149953a3835408b4a55e5361a
Nithin Joseph Simon <nithinjosephsimon@gmail.com>
2025-02-10 13:21:56 +0100

    fasta-file-processor.sh

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,6 +1,6 @@
 num_sequences=$(grep -c '>' $1)
 # To count the total length of the sequences
-total_length=$(grep -v '>' $1 | wc -c)
+total_length=$(awk '/^>/ {next}; {totallength += length $0} END{print totallength}' $1)
 #Longest seq
 length_of_the_longest_sequence=$(awk '/>/ {if (seq) print seq; print; seq=""; next} {seq=seq $0} END {print seq}' $1 | grep -v '>'| awk '{print length($0)}' | sort -n | tail -n 1)
 #Shortest seq
@@ -11,6 +11,7 @@ gc_count=$(grep -v '>' $1 | awk '{gc_count += gsub(/[GgCc]/, "", $1)} END {print
 gc_content=$(echo "scale=2; ($gc_count/$total_length)*100" | bc)
 
 echo "FASTA File Statistics:"
+echo "----------------------"
 echo "Number of sequences: $num_sequences"
 echo "Total length of sequences: $total_length"
 echo "Length of the longest sequence: $length_of_the_longest_sequence" 
